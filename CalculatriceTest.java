39e6da6e545077f3ec2a7ea9bc1b0494c1514152
DULAI Jorawar Singh <12301253@q10422.iutv.univ-paris13.fr>
2025-05-23 10:57:52 +0200

    exo 1 question 1 : add, mult, div

---
+++ CalculatriceTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import java.util.*;
+
+
+
+public class CalculatriceTest {
+
+    public Calculatrice calculatrice;
+    @BeforeEach
+    public void setUp(){
+        calculatrice = new Calculatrice();
+    }
+
+    @Test
+    public void testAdd() {
+        
+        float result = calculatrice.add(2.0f, 2.0f);
+        assertEquals(4.0f, result);
+        System.out.println("test addition ok");
+    }
+
+    @Test 
+    public void testMult() {
+        float result = calculatrice.mult(2.0f, 2.0f);
+        assertEquals(4.0f, result);
+        System.out.println("test multiplication ok");
+
+    }
+
+    @Test
+    public void testDiv() {
+        float result = calculatrice.div(2.0f, 2.0f);
+        assertEquals(1.0f, result);
+        System.out.println("test division ok");
+
+    }
+
+}
+
